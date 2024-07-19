0b2aaca7554e4bb3bbe0be9e674497a283e15484
sepehrbeyranvand <sepehr7797@gmail.com>
2024-07-19 19:18:32 +0330

    add navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
-export default function Home(){
-    return(
-        <div>
-            <h1 className="text-center">Home</h1>
+import headerImg from "../Assets/hero_image.png";
+export default function Home() {
+  return (
+    <div>
+      <header className="min-h-[48vw] bg-rose-50 w-full flex justify-between">
+        <div className="basis-1/2 flex flex-col justify-center items-start ms-20">
+          <p className="text-zinc-600 mt-7">New Arrivals Only</p>
+          <h1 className="text-[4rem] w-[70%] font-bold mt-7">
+            The Best Collections Fo the Bests
+          </h1>
+
+          <button className="w-[30%] rounded-full p-[.6rem] text-white bg-rose-800 mt-7">
+            Latest Collections
+          </button>
         </div>
-    )
+        <div>
+          <img
+            width={500}
+            height={500}
+            className="basis-1/2"
+            src={headerImg}
+            alt="Hero"
+          />
+        </div>
+      </header>
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,79 @@
-import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Routes,
+  Link,
+} from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
+import ShoppingBasket from "../Assets/shoppingbasket.png";
+
 import Logo from "../Assets/firstLogo.png";
-import Home from './Home'
-import Shop from './Shop'
-import Men from './Men'
-import Women from './Women'
-import Kids from './Kids';
+import Home from "./Home";
+import Shop from "./Shop";
+import Men from "./Men";
+import Women from "./Women";
+import Kids from "./Kids";
 export default function Navbar() {
   return (
     <div>
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/shop' element={<Shop/>}/>
-                <Route path='/men' element={<Men/>}/>
-                <Route path='/women' element={<Women/>}/>
-                <Route path='/kids' element={<Kids/>}/>
-            </Routes>
-        </Router>
+      <Router>
+        <nav className="flex justify-between items-center">
+          <div>
+            <Link to="/">
+              <img width={115} height={115} src={Logo} alt="Logo" />
+            </Link>
+          </div>
+          <div className="flex gap-10 justify-center items-center">
+            <NavLink
+              className="p-2 hover:text-rose-600 transition-all .4s ease-in-out duration-200"
+              to="/shop"
+            >
+              Shop
+            </NavLink>
+            <NavLink
+              className="p-2 hover:text-rose-600 transition-all .4s ease-in-out duration-200"
+              to="/men"
+            >
+              Men
+            </NavLink>
+            <NavLink
+              className="p-2 hover:text-rose-600 transition-all .4s ease-in-out duration-200"
+              to="/women"
+            >
+              Women
+            </NavLink>
+            <NavLink
+              className="p-2 hover:text-rose-600 transition-all .4s ease-in-out duration-200"
+              to="/kids"
+            >
+              Kids
+            </NavLink>
+          </div>
+          <div className="flex item-center justify-center p-2">
+            <button className="me-8 hover:bg-rose-50 hover:border-white transition-all .4s ease-in-out duration-200 p-2 w-[7vw] border border-1 border-zinc-900 rounded-3xl">
+              Login
+            </button>
+            <div>
+              <img
+                width={35}
+                className="object-contain bg-cover relative"
+                height={30}
+                src={ShoppingBasket}
+                alt="Cart"
+              />
+              <span className="bg-red-600 rounded-full size-2 text-center text-white absolute right-3 top-10"></span>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/men" element={<Men />} />
+          <Route path="/women" element={<Women />} />
+          <Route path="/kids" element={<Kids />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
